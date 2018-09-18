48472ef5053d1625436c19851b0bd8135e15fe7b
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-09-18 11:53:37 +0800

    Mathematical Operators: exp

---
+++ 46Mathematical_Operators_exp.jl
@@ -0,0 +1,8 @@
+# Compute the natural base exponential of x
+exp(1.0) # 2.718281828459045
+
+# Compute the base 2 exponential of x
+exp2(5) # 32.0
+
+# Compute the base 10 exponential of x,
+exp10(2) # 100.0
